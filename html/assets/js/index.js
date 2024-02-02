9a8579abb8445471cee62b0872675618f2d6b235
2002ac <ayxanceferov2002ac2gmail.com>
2024-02-02 19:34:55 +0400

    diplom-lahiyesi

---
+++ html/assets/js/index.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 15,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: true,
+  },
+loop: true,
+});
