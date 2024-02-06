1e7d0049e3b76883a545e2d557f11a425f896c25
2002ac <ayxanceferov2002ac2gmail.com>
2024-02-06 20:26:28 +0400

    diplom-lahiyesi

--- html/assets/js/detail.js
+++ html/assets/js/detail.js
@@ -11,3 +11,118 @@ closeBtn.onclick = () => {
     video.classList.remove("show-video");
     myVideo.pouse()
 }
+
+const body = document.querySelector('.cont');
+let id = new URLSearchParams(window.location.search).get("id")
+let playButton = document.querySelector('.play-movie');
+let closeBtn = document.querySelector('.close-video');
+
+fetch("http://localhost:3000/movies/" + id)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        body.innerHTML += `
+    <div class="play-container container">
+     <!-- play img -->
+      <img
+        src="../html/assets/img/movie-play-pages/barbie_slay_movie_poster_x1x.jpg_1909824714.webp"
+        alt=""
+        class="play-img"
+      />
+      <!-- play text -->
+      <div class="play-text">
+        <h2>${data.title}</h2>
+        <!-- reating -->
+        <div class="reating">
+          <i class="bx bxs-star"></i>
+          <i class="bx bxs-star"></i>
+          <i class="bx bxs-star"></i>
+          <i class="bx bxs-star"></i>
+          <i class="bx bxs-star-half"></i>
+        </div>
+        <!-- tags -->
+        <div class="tags">
+          <span>Komedi</span>
+          <span>Adventure</span>
+          <span>4k</span>
+        </div>
+        <!-- trailer button -->
+        <a href="#" class="watch-btn">
+          <i class="bx bx-right-arrow"></i>
+          <span>Watch the Trailer</span>
+        </a>
+      </div>
+      <!-- play btn -->
+      <i class="bx bx-right-arrow play-movie"></i>
+      <!-- video container -->
+      <div class="video-container">
+        <!-- video box -->
+        <div class="video-box">
+       <video id="myvideo" src="../html/assets/img/videooo/Barbie _ Teaser Trailer 2.mp4" controls></video>
+        <!-- close video -->
+        <i class="bx bx-x close-video"></i>
+      </div>
+      </div>
+    </div>
+
+    
+    <!-- about -->
+    <div class="about-movie container">
+      <h2>Barbie</h2>
+      <p>${data.desc}</p>
+       <!-- movie cast -->
+       <h2 class="cast-heading">Movie Cast</h2>
+       <div class="cast">
+        <div class="cast-box">
+          <img src="/html/assets/img/movie-play pages/barbiyeoyuncular/margotRobbie.webp" alt="" class="cast-img">
+          <span class="cast-title">Margot Robbie</span>
+        </div>
+        <div class="cast-box">
+          <img src="/html/assets/img/movie-play pages/barbiyeoyuncular/arianaGreeenblat.webp" alt="" class="cast-img">
+          <span class="cast-title">Ariana Greenblatt</span>
+        </div>
+        <div class="cast-box">
+          <img src="/html/assets/img/movie-play pages/barbiyeoyuncular/KingsleyBenAdin.webp" alt="" class="cast-img">
+          <span class="cast-title">Kingsley Ben-Adir</span>
+        </div>
+        <div class="cast-box">
+          <img src="/html/assets/img/movie-play pages/barbiyeoyuncular/ryanGosling.webp" alt="" class="cast-img">
+          <span class="cast-title">Ryan Gosling</span>
+        </div>
+        <div class="cast-box">
+          <img src="/html/assets/img/movie-play pages/barbiyeoyuncular/IsaRea.webp" alt="" class="cast-img">
+          <span class="cast-title">Isa Rea</span>
+        </div>
+        <!-- <div class="cast-box">
+          <img src="/html/assets/img/movie-play pages/barbiyeoyuncular/americaFerrea.webp" alt="" class="cast-img">
+          <span class="cast-title">America Ferrea</span>
+        </div> -->
+        <div class="cast-box">
+          <img src="/html/assets/img/movie-play pages/barbiyeoyuncular/MichelCera.webp" alt="" class="cast-img">
+          <span class="cast-title">Michel Cera</span>
+        </div>
+       </div>
+    </div>
+`
+    })
+    fetch("http://localhost:3000/movies/" + id)
+    .then(res => res.json())
+    .then(data => {
+        data.actors.forEach(item => {
+            body.innerHTML += `
+        <!-- about -->
+        <div class="about-movie container">
+          <h2>Barbie</h2>
+          <p>${data.desc}</p>
+           <!-- movie cast -->
+           <h2 class="cast-heading">Movie Cast</h2>
+           <div class="cast">
+            <div class="cast-box">
+              <img src="/html/assets/img/movie-play pages/barbiyeoyuncular/margotRobbie.webp" alt="" class="cast-img">
+              <span class="cast-title">Margot Robbie</span>
+            </div>
+           </div>
+        </div>
+        `
+        })
+    })
--- html/assets/js/index.js
+++ html/assets/js/index.js
@@ -24,3 +24,32 @@ var swiper = new Swiper(".mySwiper", {
   },
 loop: true,
 });
+
+const movieContent = document.querySelector(".movies-content")
+
+const getMovies = function(){
+  movieContent.innerHTML =""
+  fetch("http://localhost:3000/movies")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(item => {
+      movieContent.innerHTML += `
+      <div class="movie-box">
+      <img
+        src="../html/assets/img/movelistitem/${item.image}"
+        alt=""
+      />
+      <div class="box-movie-text">
+        <h2 class="Box-movie-title">${item.title}</h2>
+        <span class="s-movie-type">${item.genre}</span>
+        <a href="../html/details-play.html?id=${item.id}" class="watch-btn play-btn">
+          <i class="bx bx-right-arrow"></i>
+        </a>
+      </div>
+    </div>
+      `
+    })
+  })
+}
+
+getMovies()
