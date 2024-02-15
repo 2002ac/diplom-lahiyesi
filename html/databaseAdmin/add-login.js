7310582658c3710610df7f67c59c36976b59b91a
2002ac <ayxanceferov2002ac2gmail.com>
2024-02-15 23:29:20 +0400

    diplom-lahiyesi

--- html/databaseAdmin/add-login.js
+++ html/databaseAdmin/add-login.js
@@ -1,22 +1,9 @@
-let titleInput = document.querySelector(".title");
-let genreInput = document.querySelector(".genre");
-let imageInput = document.querySelector(".Image");
-let movieBox = document.querySelector(".movie-box");
+
 let table = document.querySelector("table");
-let addBtn = document.querySelector(".addbtn");
 
 
 
-imageInput.addEventListener('input', (e) => {
-  let file = e.target.files[0];
-  if (file) {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-        movieBox.src = reader.result;
-    }
-  }
-})
+
 
 
 fetch(" http://localhost:3000/login")
